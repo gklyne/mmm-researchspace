1ae2740eb94ea8fadc9be0552bd5b96254e709d2
Graham Klyne <gk-github@ninebynine.org>
2020-02-01 09:20:35 +0000

    Simplify build sequence

--- researchspace/build-researchspace-image.sh
+++ researchspace/build-researchspace-image.sh
@@ -7,6 +7,6 @@ echo " "
 echo "docker run -it --network=host researchspace"
 echo " "
 echo "Use 'ifconfig' in the running system to find the IP address for 'eth1'"
-echo "Run '. start-research.sh' in the running system."
+echo "Run '. start-researchspace.sh' in the running system."
 echo "Browse to 'http://<IP-address>:10214' from the host system."
 
