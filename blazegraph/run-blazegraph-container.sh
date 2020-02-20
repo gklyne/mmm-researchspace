399e0e842018e9e42975657cf67fe48e0f4eec86
Graham Klyne <gk-github@ninebynine.org>
2020-02-20 10:54:47 +0000

    Update to use latest Docker and NAT network on MacOS

--- blazegraph/run-blazegraph-container.sh
+++ blazegraph/run-blazegraph-container.sh
@@ -4,5 +4,5 @@ echo "Use 'ifconfig' in the running system to find the IP address for 'eth1'"
 echo "Run '. start-blazeraph.sh' in the running system.  Ignore the displayed IP address."
 echo "Browse to 'http://<IP-address>:9999/blazegraph/' from the host system."
 
-docker run -it --network=host blazegraph
-
+# docker run -it --network=host blazegraph
+docker run -it --publish=9999:9999 blazegraph
--- researchspace/run-researchspace-container.sh
+++ researchspace/run-researchspace-container.sh
@@ -1,1 +1,2 @@
-docker run -it --network=host researchspace
+# docker run -it --network=host researchspace
+docker run -it --publish=10214:10214 researchspace
